42651e168a4719a0b2943c0922b2439625b08a47
Unknown <adi.bukowski@gmail.com>
2018-06-10 16:32:42 +0200

    errors fixed

--- simplex.py
+++ simplex.py
@@ -5,15 +5,7 @@ Created on Mon May 28 12:05:03 2018
 @author: Adrian
 """
 import numpy as np
-n=5
-m=10
-D=np.array([[5,7,35],[-1,2,2],[3,-6,0]])
-x=['x1','x2']
-z=['z1','z2']
 
-A = np.array([[0 for i in range(m)] for i in range(n)])
-b = np.array([[0 for i in range(n)]])
-c= np.array([[0 for i in range(m)]])
 
 def simplex_matrix(A,b,c):
     n,m = A.shape
@@ -42,7 +34,6 @@ def move(D,x,z,i0,j0):
     x[j0]=z[i0]
     z[i0]=temp_var
     return([A,x,z])
-D=np.array([[-1,0,-1],[0,1,5],[-1,-1,-4],[1,-1,2],[2,1,6],[-1,-2,0]])
 
 def simplex(D):
     n,m = D.shape
@@ -53,14 +44,13 @@ def simplex(D):
     for i in range(1,n):
         z.append('z'+str(i))
     while((D[:,m-1].astype(float)<0).any()==True):
-        neg_values_pos=[i for i, elem in enumerate(D[:,m-1].astype(float)) if elem<0]
-        i0=neg_values_pos[-1]
-        if(((D[i0,:-1].astype(float)<0).any())==False):
+        i0_temp=n-list(reversed(D[:,m-1].astype(float)<0)).index(1)-1
+        if(((D[i0_temp,:-1].astype(float)<0).any())==False):
             return('Nie ma rozwiazan dopuszczalnych')
         else:
-            j0=np.argmax(D[i0,:-1].astype(float)<0)
-            temp_vec = D[i0:,j0].astype(float)/(D[i0:,m-2].astype(float))
-            i0=np.argmax(temp_vec)
+            j0=(D[i0_temp,:-1].astype(float)<0).tolist().index(1)
+            temp_vec = D[i0_temp:-1,j0].astype(float)/(D[i0_temp:-1,m-1].astype(float))
+            i0=i0_temp+np.argmax(temp_vec)
             lst=move(D,x,z,i0,j0)
             D=lst[0]
             x=lst[1]
@@ -68,10 +58,10 @@ def simplex(D):
             print(D)
     while((D[n-1,:-1].astype(float)<0).any()==True):
         j0=np.argmax(D[-1,:-1].astype(float)<0)
-        if(((D[1:,j0].astype(float)<=0).any())==False):
+        if(((D[:-1,j0].astype(float)>0).any())==False):
             return('Funkcja celu jest nieograniczona')
         else:
-            temp_vec = D[1:,j0].astype(float)/D[n-1,j0].astype(float)
+            temp_vec = D[:-1,j0].astype(float)/D[:-1,m-1].astype(float)*(D[:-1,j0].astype(float)>0)
             i0=np.argmax(temp_vec)
             lst=move(D,x,z,i0,j0)
             D=lst[0]
@@ -86,5 +76,6 @@ def simplex(D):
         else:
             x_best.append(D[z.index(xIndex),-1])
     return([D[n-1,m-1],x_best])
-    
+
+D=np.array([[-1,0,-1],[0,1,5],[-1,-1,-4],[1,-1,2],[2,1,6],[-1,-2,0]])
 simplex(D)
